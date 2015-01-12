21f87ce35b72774c3b49b6f1d1be5493fa9245be
Hien <nvhien1992@gmail.com>
2015-01-12 10:34:45 +0700

    add test project

---
+++ test/Sources/mqx_tasks.c
@@ -0,0 +1,88 @@
+/* ###################################################################
+**     Filename    : mqx_tasks.c
+**     Project     : test
+**     Processor   : MKE06Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-01-10, 20:34, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Test_task - void Test_task(uint32_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file mqx_tasks.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup mqx_tasks_module mqx_tasks module documentation
+**  @{
+*/         
+/* MODULE mqx_tasks */
+
+#include "Cpu.h"
+#include "Events.h"
+#include "mqx_tasks.h"
+#include "Init_Config.h"
+#include "PDD_Includes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+#include <stdio.h>
+
+/*
+** ===================================================================
+**     Event       :  Test_task (module mqx_tasks)
+**
+**     Component   :  Task1 [MQXLite_task]
+**     Description :
+**         MQX task routine. The routine is generated into mqx_tasks.c
+**         file.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         task_init_data  - 
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Test_task(uint32_t task_init_data)
+{
+  int counter = 0;
+
+  while(1) {
+    counter++;
+
+    /* Write your code here ... */
+    printf("=== Test_task ===\n");
+    GPIO1_ToggleFieldBits(NULL, led_red, 0xFFFFFFFF);
+    _time_delay_ticks(100);
+  }
+}
+
+/* END mqx_tasks */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
